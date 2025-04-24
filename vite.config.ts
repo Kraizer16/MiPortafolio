a6b835a6fc13160fb9e8ac8ec36c85668e7b826d
marloncg <marlonacg1603@gmail.com>
2025-04-23 19:32:48 -0500

    correcionGH

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https//kraizer16.github.io/MiPortafolio',
+  base:'https//Kraizer16.github.io/MiPortafolio',
 })
